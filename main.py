9d5f25bca4c94665701fd9a82dd8c0ee1b18f5c8
jaerrib <jaerrib@tutanota.com>
2023-01-19 18:56:07 -0500

    Limit range of converted frequencies

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import wavio
 import argparse
+from math import trunc
 
 
 def image_to_array(save_path):
@@ -11,8 +12,44 @@ def image_to_array(save_path):
 
 
 def rgb_to_frequency(r, g, b):
-    freq = (r * g * b) / 829.06875
-    return freq
+    red_dict = {
+        "0": 130.8128,
+        "1": 146.8324,
+        "2": 164.8138,
+        "3": 174.6141,
+        "4": 195.9977,
+        "5": 220.0000,
+        "6": 246.9417,
+    }
+
+    green_dict = {
+        "0": 261.6256,
+        "1": 293.6646,
+        "2": 329.6276,
+        "3": 349.2282,
+        "4": 391.9954,
+        "5": 440.0000,
+        "6": 493.8833,
+    }
+
+    blue_dict = {
+        "0": 523.2511,
+        "1": 587.3295,
+        "2": 659.4565,
+        "3": 698.4565,
+        "4": 783.9909,
+        "5": 880.0000,
+        "6": 987.7666,
+    }
+
+    main_color = max(r, g, b)
+    key_item = str(trunc(main_color / 37))
+    if main_color == r:
+        return red_dict[key_item]
+    elif main_color == g:
+        return green_dict[key_item]
+    elif main_color == b:
+        return blue_dict[key_item]
 
 
 def array_to_sound(array):
