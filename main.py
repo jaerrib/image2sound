351c4c9388d9dbfcccf5e5632cdd6bbaf2a0e14c
jaerrib <jaerrib@tutanota.com>
2023-01-29 10:53:08 -0500

    Modify color to frequency conversion

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def image_to_array(save_path):
     return img_arr
 
 
-def rgb_to_frequency(r, g, b):
-    red_dict = {
+def get_freq(color1, color2):
+    freq_dict = {
         "0": 130.8128,
         "1": 146.8324,
         "2": 164.8138,
@@ -20,36 +20,39 @@ def rgb_to_frequency(r, g, b):
         "4": 195.9977,
         "5": 220.0000,
         "6": 246.9417,
+        "7": 261.6256,
+        "8": 293.6646,
+        "9": 329.6276,
+        "10": 349.2282,
+        "11": 391.9954,
+        "12": 440.0000,
+        "13": 493.8833,
+        "14": 523.2511,
+        "15": 587.3295,
+        "16": 659.4565,
+        "17": 698.4565,
+        "18": 783.9909,
+        "19": 880.0000,
+        "20": 987.7666,
+        "21": 1046.502,
+        "22": 1174.659,
+        "23": 1318.510,
+        "24": 1396.913,
+        "25": 1567.982,
+        "26": 1760.000,
+        "27": 1975.533,
+        "28": 2093.005,
+        "29": 2349.318,
+        "30": 2637.020,
+        "31": 2793.826,
+        "32": 3135.963,
+        "33": 3520.000,
+        "34": 3951.066,
     }
 
-    green_dict = {
-        "0": 261.6256,
-        "1": 293.6646,
-        "2": 329.6276,
-        "3": 349.2282,
-        "4": 391.9954,
-        "5": 440.0000,
-        "6": 493.8833,
-    }
-
-    blue_dict = {
-        "0": 523.2511,
-        "1": 587.3295,
-        "2": 659.4565,
-        "3": 698.4565,
-        "4": 783.9909,
-        "5": 880.0000,
-        "6": 987.7666,
-    }
-
-    main_color = max(r, g, b)
-    key_item = str(trunc(main_color / 37))
-    if main_color == r:
-        return red_dict[key_item]
-    elif main_color == g:
-        return green_dict[key_item]
-    elif main_color == b:
-        return blue_dict[key_item]
+    selection = (color1 + color2) / 2
+    key_item = str(trunc(selection / 7.314285714))
+    return freq_dict[key_item]
 
 
 def array_to_sound(array):
@@ -59,9 +62,9 @@ def array_to_sound(array):
             red = y[0]
             green = y[1]
             blue = y[2]
-            freq = rgb_to_frequency(red, green, blue)
-            tone = np.sin(2 * np.pi * freq)
-            temp_array.append([tone, tone])
+            left_tone = np.sin(2 * np.pi * get_freq(red, green))
+            right_tone = np.sin(2 * np.pi * get_freq(green, blue))
+            temp_array.append([left_tone, right_tone])
     return temp_array
 
 
