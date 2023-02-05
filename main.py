0d75919424ab8dc6360e9b50d96cb9e5f1421704
jaerrib <jaerrib@tutanota.com>
2023-02-05 16:57:14 -0500

    Add ability to choose tempo - Closes #2

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from sound_image import SoundImage
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", type=str)
 parser.add_argument("-key", "--key", type=str)
+parser.add_argument("-t", "--tempo", type=str)
 args = parser.parse_args()
 path = args.path
 key = args.key
-si = SoundImage(path, key)
+tempo = args.tempo
+si = SoundImage(path, key, tempo)
 si.convert_to_multiple()
--- sound2image.py
+++ sound2image.py
@@ -1,72 +1,62 @@
-import numpy as np
 from PIL import Image
-from scipy.io import wavfile
-from math import sqrt
-import argparse
+import numpy as np
+import wavio
+from math import trunc
+from tone_array import get_tone_array
 
 
-def get_channel_value(num):
-    """
-    Converts the numerical data taken from the WAV and converts it to a number
-    between 0 and 255
-    """
-    channel_value = int(abs(num / 3871999.2109375))
-    return channel_value
+class SoundImage:
 
+    def __init__(self, path, key="C", tempo="60"):
+        self.path = path
+        self.freq_dict = get_tone_array(key)
+        self.image_array = self.image_to_array(path)
+        self.length = len(self.freq_dict)
+        self.tempo = int(tempo)
 
-def get_color_array(file_path):
-    """
-    Accepts a path to a 24-bit WAV file, converts it to a numpy array, then
-    subsequently converts the left channel to a red value, the right channel to
-    a blue value, and the average of te left and right to green. Returns an
-    array made up of the resulting RGB values.
-    """
-    output = wavfile.read(file_path)
-    array = np.asarray(output[1])
-    array_length = int(array.size / 2)
-    temp_array = []
-    for i in range(0, array_length):
-        red = get_channel_value(array[i][0])
-        green = get_channel_value((array[i][0] + array[i][1]) / 2)
-        blue = get_channel_value(array[i][1])
-        temp_array.append([red, green, blue])
-    return temp_array
+    @staticmethod
+    def image_to_array(path):
+        img = Image.open(path).convert(mode="RGB")
+        img_arr = np.asarray(img, dtype='int64')
+        return img_arr
 
+    def get_freq(self, color):
+        divisor = 256 / self.length
+        key_item = int(trunc(color / divisor))
+        return self.freq_dict[key_item]
 
-def make_sq_img(array):
-    """
-    Accepts the color array and rearranges it as a square image array
-    """
-    size = int(sqrt(len(array)))
-    count = 0
-    img_array = []
-    for row_num in range(size):
-        row = []
-        for col_num in range(size):
-            row.append([0, 0, 0])
-        img_array.append(row)
-    for j in range(size):
-        for k in range(size):
-            img_array[j][k] = array[count]
-            count += 1
-    return img_array
+    def get_sin(self, color):
+        freq = self.get_freq(color)
+        rate = 44100
+        time = 60 / self.tempo
+        n = int(rate * time)
+        time_grid = np.arange(n) / rate
+        return np.sin(2 * np.pi * freq * time_grid)
 
+    @staticmethod
+    def save_wav(save_path, side, array):
+        rate = 44100
+        split_str = save_path.split(".")
+        split_str = split_str[0].split("/")
+        file_name = split_str[-1] + side + ".wav"
+        wavio.write(file_name, array, rate, scale=2, sampwidth=3, clip="ignore")
+        print("Saved file as ", file_name)
 
-def save_wav(save_path, array):
-    image = Image.fromarray(array.astype("uint8"), "RGB")
-    maxsize = (1000, 1000)
-    image.thumbnail(maxsize, Image.LANCZOS)
-    split_str = save_path.split(".")
-    split_str = split_str[0].split("/")
-    file_name = split_str[-1]+".jpg"
-    image.save(file_name)
-    print("Saved file as ", file_name)
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--path", type=str)
-args = parser.parse_args()
-path = args.path
-color_array = get_color_array(path)
-square_image = np.array(make_sq_img(color_array))
-save_wav(path, square_image)
+    def convert_to_multiple(self):
+        red_array = []
+        green_array = []
+        blue_array = []
+        for x in self.image_array:
+            for y in x:
+                red = y[0]
+                green = y[1]
+                blue = y[2]
+                red_value = self.get_sin(red)
+                green_value = self.get_sin(green)
+                blue_value = self.get_sin(blue)
+                red_array.append([red_value])
+                green_array.append([green_value])
+                blue_array.append([blue_value])
+        self.save_wav(self.path, "-R", red_array)
+        self.save_wav(self.path, "-G", green_array)
+        self.save_wav(self.path, "-B", blue_array)
