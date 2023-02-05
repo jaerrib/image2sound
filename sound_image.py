c7e6d855d6a834430f58d3f5233a893d4367c602
jaerrib <jaerrib@tutanota.com>
2023-02-05 10:51:02 -0500

    Add ability to choose key - Closes #5 - Closes #1

--- sound_image.py
+++ sound_image.py
@@ -2,14 +2,16 @@ from PIL import Image
 import numpy as np
 import wavio
 from math import trunc
+from tone_array import get_tone_array
 
 
 class SoundImage:
 
-    def __init__(self, path, key="C"):
+    def __init__(self, path, key):
         self.path = path
-        self.key = key
+        self.freq_dict = get_tone_array(key)
         self.image_array = self.image_to_array(path)
+        self.length = len(self.freq_dict)
 
     @staticmethod
     def image_to_array(path):
@@ -17,47 +19,10 @@ class SoundImage:
         img_arr = np.asarray(img, dtype='int64')
         return img_arr
 
-    @staticmethod
-    def get_freq(color):
-        freq_dict = {
-            "0": 130.8128,
-            "1": 146.8324,
-            "2": 164.8138,
-            "3": 174.6141,
-            "4": 195.9977,
-            "5": 220.0000,
-            "6": 246.9417,
-            "7": 261.6256,
-            "8": 293.6646,
-            "9": 329.6276,
-            "10": 349.2282,
-            "11": 391.9954,
-            "12": 440.0000,
-            "13": 493.8833,
-            "14": 523.2511,
-            "15": 587.3295,
-            "16": 659.4565,
-            "17": 698.4565,
-            "18": 783.9909,
-            "19": 880.0000,
-            "20": 987.7666,
-            "21": 1046.502,
-            "22": 1174.659,
-            "23": 1318.510,
-            "24": 1396.913,
-            "25": 1567.982,
-            "26": 1760.000,
-            "27": 1975.533,
-            "28": 2093.005,
-            "29": 2349.318,
-            "30": 2637.020,
-            "31": 2793.826,
-            "32": 3135.963,
-            "33": 3520.000,
-            "34": 3951.066,
-        }
-        key_item = str(trunc(color / 7.314285714))
-        return freq_dict[key_item]
+    def get_freq(self, color):
+        divisor = 256 / self.length
+        key_item = int(trunc(color / divisor))
+        return self.freq_dict[key_item]
 
     def get_sin(self, color):
         freq = self.get_freq(color)
