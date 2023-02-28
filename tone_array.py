6380ccf4129470d0614953d03db45ff78d68efa9
jaerrib <jaerrib@tutanota.com>
2023-02-28 12:57:33 -0500

    Remove hardcoded scale index length - Prepare for scale types less than 7 notes (pentatonic, for example)

--- tone_array.py
+++ tone_array.py
@@ -134,7 +134,8 @@ SCALE = {
 def get_tone_array(key):
     scale_array = SCALE[key]
     tone_array = []
-    for index in range(1, 8):
+    index_length = len(scale_array) + 1
+    for index in range(1, index_length):
         for note in scale_array:
             tone_array.append(FREQ_DICT[note+str(index)])
     return tone_array
