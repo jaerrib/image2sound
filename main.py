9773d81151b36f8af70cd195cd301a08c8b796c4
jaerrib <jaerrib@tutanota.com>
2023-06-04 07:03:25 -0400

    Add target track length parameters

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", type=str)
 parser.add_argument("-key", "--key", type=str)
 parser.add_argument("-t", "--tempo", type=str)
+parser.add_argument("-min", "--minutes", type=str)
+parser.add_argument("-sec", "--seconds", type=str)
 args = parser.parse_args()
 
 kwargs = {
     "path": args.path,
     "key": args.key,
     "tempo": args.tempo,
+    "minutes": args.minutes,
+    "seconds": args.seconds,
 }
 
 si = SoundImage(**{key: val for key, val in kwargs.items() if val is not None})
