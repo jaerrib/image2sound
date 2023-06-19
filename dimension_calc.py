20ee40c5cfad6bb9a21960fc93ecc8e626bed642
gregorni <gregorniehl@web.de>
2023-06-19 16:40:16 +0200

    Calculate track time right after parsing arguments

--- dimension_calc.py
+++ dimension_calc.py
@@ -1,8 +1,8 @@
 from math import sqrt, floor
 
 
-def get_new_dim(img_dim, minutes, seconds, tempo):
-    target_pixels = (minutes + (seconds / 60)) * tempo
+def get_new_dim(img_dim, time_in_minutes, tempo):
+    target_pixels = time_in_minutes * tempo
     img_ratio = img_dim[0] / img_dim[1]
     new_height = floor(sqrt(target_pixels / img_ratio))
     new_width = floor(new_height * img_ratio)
