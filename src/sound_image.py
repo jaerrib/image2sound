2f52fc725f575a2f5b0fdad5f39f4fcfed0dcb5b
jaerrib <jaerrib@tutanota.com>
2025-01-26 09:24:14 -0500

    Add type hinting to improve IDE experience

--- src/sound_image.py
+++ src/sound_image.py
@@ -1,10 +1,12 @@
 import math
 import os
+from typing import Self
 
 import numpy as np
 import wavio
 from PIL import Image
 from halo import Halo
+# noinspection PyProtectedMember
 from mutagen.id3 import APIC
 from mutagen.wave import WAVE
 
@@ -12,8 +14,8 @@ import dimension_calc
 import tone_array
 from envelope_settings import envelope_settings
 
-RATE = 44100
-DEFAULT_SETTINGS = {
+RATE: int = 44100
+DEFAULT_SETTINGS: dict = {
     "path": "test_image.png",
     "output": "",
     "key": "C-Major",
@@ -47,31 +49,31 @@ class SoundImage:
         time_signature,
         adsr,
         waveform,
-    ):
-        self.path = path
-        self.output = output
-        self.key = key
+    ) -> None:
+        self.path: str = path
+        self.output: str = output
+        self.key: str = key
         self.freq_dict = tone_array.get_tone_array(self.key)
         self.length = len(self.freq_dict)
-        self.time_signature = self.separate_time_signature(time_signature)
-        self.tempo = tempo
-        self.note_length = self.get_note_length()
-        self.minutes = minutes + seconds / 60
+        self.time_signature: list[int] = self.separate_time_signature(time_signature)
+        self.tempo: int = tempo
+        self.note_length: float = self.get_note_length()
+        self.minutes: int = minutes + seconds / 60
         self.image_array = None
-        self.split = split
-        self.reveal = reveal
+        self.split: bool = split
+        self.reveal: bool = reveal
         self.overrides = overrides
-        self.method2 = method2
-        self.smooth = smooth
+        self.method2: bool = method2
+        self.smooth: bool = smooth
         self.image_mode = None
-        self.adsr = adsr
-        self.adsr_settings = envelope_settings[self.adsr]
-        self.waveform = waveform
+        self.adsr: str = adsr
+        self.adsr_settings: dict = envelope_settings[self.adsr]
+        self.waveform: str = waveform
 
-    def open_file(self):
+    def open_file(self) -> Image.Image:
         return Image.open(self.path)
 
-    def image_to_array(self, img):
+    def image_to_array(self, img: Image.Image) -> Self:
         self.image_array = np.asarray(
             img.resize(
                 dimension_calc.get_new_dim(
@@ -82,13 +84,13 @@ class SoundImage:
         )
         return self
 
-    def get_note_length(self):
+    def get_note_length(self) -> float:
         duration = 60 / self.tempo
         note_length = duration / self.time_signature[1]
         return note_length
 
     @staticmethod
-    def separate_time_signature(time_signature):
+    def separate_time_signature(time_signature: str) -> list[int]:
         top, bottom = time_signature.split("/")
         top, bottom = int(top), int(bottom)
         if top == 0:
@@ -99,29 +101,29 @@ class SoundImage:
         return split_time_signature
 
     @staticmethod
-    def get_freq(color, freq_range):
+    def get_freq(color: int, freq_range) -> float:
         return freq_range[math.trunc(color / (256 / len(freq_range))) - 1]
 
     @staticmethod
-    def apply_blackman(wave):
+    def apply_blackman(wave: np.ndarray) -> np.ndarray:
         # Optionally apply the Blackman smoothing window
         blackman_window = np.blackman(len(wave))
         wave *= blackman_window
         return wave
 
-    def get_envelope(self, amplitude):
-        attack = self.adsr_settings["a"] * self.note_length
-        decay = self.adsr_settings["d"] * self.note_length
-        sustain = self.adsr_settings["s"] * amplitude
-        sustain_level = sustain
-        release = self.adsr_settings["r"] * self.note_length
-        sample_rate = RATE
+    def get_envelope(self, amplitude: float) -> np.ndarray:
+        attack: float = self.adsr_settings["a"] * self.note_length
+        decay: float = self.adsr_settings["d"] * self.note_length
+        sustain: float = self.adsr_settings["s"] * amplitude
+        sustain_level: float = sustain
+        release: float = self.adsr_settings["r"] * self.note_length
+        sample_rate: int = RATE
 
         total_samples = int(RATE * self.note_length)
         attack_samples = int(attack * sample_rate)
         decay_samples = int(decay * sample_rate)
         release_samples = int(release * sample_rate)
-        sustain_samples = total_samples - (
+        sustain_samples: int = total_samples - (
             attack_samples + decay_samples + release_samples
         )
 
@@ -140,7 +142,9 @@ class SoundImage:
 
         return envelope
 
-    def get_wave(self, color, freq_range, amplitude, wave_type):
+    def get_wave(
+        self, color: int, freq_range: list[float], amplitude: float, wave_type: str
+    ) -> np.ndarray:
         freq = self.get_freq(color, freq_range)
         t = np.linspace(
             0, self.note_length, int(RATE * self.note_length), endpoint=False
@@ -189,7 +193,7 @@ class SoundImage:
         return wave
 
     @staticmethod
-    def save_wav(input_path, output_path, side, array):
+    def save_wav(input_path: str, output_path: str, side: str, array) -> None:
         file_name = ".".join(input_path.split(".")[:-1]).split("/")[-1] + side + ".wav"
         if output_path == "":
             pass
@@ -213,13 +217,13 @@ class SoundImage:
             audio_file.save()
         print("Saved file as " + file_name)
 
-    def get_amplitude(self, index):
+    def get_amplitude(self, index: int) -> float:
         # Accents the first beat of each measure by applying a higher amplitude
         if index % self.time_signature[0] == 0:
             return 0.7
         return 0.5
 
-    def convert_to_multiple(self):
+    def convert_to_multiple(self) -> None:
         with Halo(text="Converting data…", color="white"):
             for char in self.image_mode:
                 if self.method2:
@@ -260,7 +264,7 @@ class SoundImage:
                     np.hstack((np.array(color_array).reshape(-1, 1),)),
                 )
 
-    def convert_to_stereo(self):
+    def convert_to_stereo(self) -> None:
         if self.method2:
             left_freq_range = []
             right_freq_range = []
@@ -307,7 +311,7 @@ class SoundImage:
             ),
         )
 
-    def convert(self):
+    def convert(self) -> None:
         img = self.open_file()
         if img.mode not in ["RGB", "RGBA", "CMYK"]:
             print("Invalid image type. Please use an RGB, RGBA, or CMYK file.")
@@ -324,7 +328,7 @@ class SoundImage:
             else:
                 self.convert_to_stereo()
 
-    def determine_key(self, red, green, blue):
+    def determine_key(self, red: int, green: int, blue: int) -> Self:
         notes = tone_array.get_chromatic_notes()
         key = notes[math.trunc(red / (255 / len(notes)))] + (
             "Major" if blue % 2 == 0 else "Minor"
@@ -336,11 +340,11 @@ class SoundImage:
         self.key = key
         return self
 
-    def override(self, img):
+    def override(self, img: Image.Image) -> Self:
         tiny_img_arr = np.asarray(img.resize((1, 1)), dtype="int64")
-        red = tiny_img_arr[0][0][0]
-        green = tiny_img_arr[0][0][1]
-        blue = tiny_img_arr[0][0][2]
+        red = int(tiny_img_arr[0][0][0])
+        green = int(tiny_img_arr[0][0][1])
+        blue = int(tiny_img_arr[0][0][2])
         if "tempo" in self.overrides:
             self.tempo = (red + green + blue) / 3
         if "key" in self.overrides:
@@ -349,7 +353,7 @@ class SoundImage:
             self.minutes = math.sqrt((img.size[0] + img.size[1]) / 2) / 2
         return self
 
-    def create_quartet(self):
+    def create_quartet(self) -> None:
         cyan_freq_range = []
         magenta_freq_range = []
         yellow_freq_range = []
